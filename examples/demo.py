40289f2938b871e7e71f0c280a8ddcac716f0a1d
Vassilis Choutas <vassilis.choutas@tuebingen.mpg.de>
2019-05-10 10:56:19 +0200

    Create example script

---
+++ examples/demo.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+
+import os.path as osp
+import argparse
+
+import numpy as np
+import torch
+
+import pyrender
+import trimesh
+import smplx
+
+
+def main(model_folder, gender='neutral', plot_joints=False,
+         use_face_contour=False):
+
+    model = smplx.SMPLX(model_folder, ext='pkl', gender=gender,
+                        use_face_contour=use_face_contour)
+    print(model)
+
+    betas = torch.randn([1, 10], dtype=torch.float32)
+    expression = torch.randn([1, 10], dtype=torch.float32)
+
+    output = model(betas=betas, expression=expression,
+                   return_verts=True)
+    vertices = output.vertices.detach().cpu().numpy().squeeze()
+    joints = output.joints.detach().cpu().numpy().squeeze()
+
+    print('Vertices shape =', vertices.shape)
+    print('Joints shape =', joints.shape)
+
+    vertex_colors = np.ones([vertices.shape[0], 4]) * [0.3, 0.3, 0.3, 0.8]
+    tri_mesh = trimesh.Trimesh(vertices, model.faces,
+                               vertex_colors=vertex_colors)
+
+    mesh = pyrender.Mesh.from_trimesh(tri_mesh)
+
+    scene = pyrender.Scene()
+    scene.add(mesh)
+
+    if plot_joints:
+        sm = trimesh.creation.uv_sphere(radius=0.005)
+        sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
+        tfs = np.tile(np.eye(4), (len(joints), 1, 1))
+        tfs[:, :3, 3] = joints
+        joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
+        scene.add(joints_pcl)
+
+    pyrender.Viewer(scene, use_raymond_lighting=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='SMPL-X Demo')
+
+    parser.add_argument('--model-folder', required=True, type=str,
+                        help='The path to the model folder')
+    parser.add_argument('--gender', type=str, default='neutral',
+                        help='The gender of the model')
+    parser.add_argument('--plot-joints', default=False,
+                        type=lambda arg: arg.lower() in ['true', '1'],
+                        help='The path to the model folder')
+    parser.add_argument('--use-face-contour', default=False,
+                        type=lambda arg: arg.lower() in ['true', '1'],
+                        help='Compute the contour of the face')
+
+    args = parser.parse_args()
+
+    model_folder = osp.expanduser(osp.expandvars(args.model_folder))
+    plot_joints = args.plot_joints
+    use_face_contour = args.use_face_contour
+    gender = args.gender
+
+    main(model_folder, gender=gender, plot_joints=plot_joints,
+         use_face_contour=use_face_contour)
