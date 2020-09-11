e821375eef0e11e3eab9d9ff0d9ad3a9a1066909
vchoutas <vchoutas@users.noreply.github.com>
2020-09-11 10:41:53 +0200

    Bump version to include new fixes

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ URL = 'http://smpl-x.is.tuebingen.mpg.de'
 EMAIL = 'vassilis.choutas@tuebingen.mpg.de'
 AUTHOR = 'Vassilis Choutas'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.21'
+VERSION = '0.1.22'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
