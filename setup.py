1fc470c71605f0c360fb6822106c9808b5a76d0f
vchoutas <vchoutas@users.noreply.github.com>
2020-10-19 15:56:44 +0200

    Add dataclasses to requirements.txt

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ URL = 'http://smpl-x.is.tuebingen.mpg.de'
 EMAIL = 'vassilis.choutas@tuebingen.mpg.de'
 AUTHOR = 'Vassilis Choutas'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.25'
+VERSION = '0.1.26'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
