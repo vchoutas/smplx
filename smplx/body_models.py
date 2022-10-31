2066b65d8a0c5935bf1dc4df6943781df4312f92
Jing Li <lijing.coder@qq.com>
2022-10-31 18:39:42 +0800

    bug fix
    expression should be expanded when the batch size larger than 1

--- smplx/body_models.py
+++ smplx/body_models.py
@@ -1235,6 +1235,7 @@ class SMPLX(SMPLH):
         scale = int(batch_size / betas.shape[0])
         if scale > 1:
             betas = betas.expand(scale, -1)
+            expression = expression.expand(scale, -1)
         shape_components = torch.cat([betas, expression], dim=-1)
 
         shapedirs = torch.cat([self.shapedirs, self.expr_dirs], dim=-1)
